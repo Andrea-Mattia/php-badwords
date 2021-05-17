78eb117d4d3d5ee9735a7f470fca58e08d2091c5
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-05-17 17:29:54 +0200

    add text var and printed text and length

--- index.php
+++ index.php
@@ -7,8 +7,24 @@
     <title>PHP Badwords</title>
 </head>
 <body>
+
+    <?php
+    // DESCRIZIONE:
+    // Creare una variabile con assegnato un testo per popolare un paragrafo.
+    // Visualizzare a schermo il paragrafo.
+    // Visualizzare a schermo la lunghezza di testo del paragrafo.
+    // Passare un parametro (query string) nella url chiamato badword con assegnato il valore di una parola scelta da voi, ottenere il suo valore con GET in PHP e sostituire ogni ricorrenza di questa parola, all'interno del testo creato da voi sopra, con tre *.
+    // Stampare a schermo il paragrafo con il testo censurato.
+
+    $name = 'Andrea';
+    $text = 'Lorem ipsum foffa dolor sit amet, consectetur foffa';
+    $length = strlen($text);
+    ?>
+
     <header>
-        <h1>Welcome boi</h1>
+        <h1>Welcome <?php echo $name; ?></h1>
+        <p>Il testo è: <?php echo $text; ?></p>
+        <p>La lungezza del testo: "<?php echo $text; ?>" è: <?php echo $length; ?> caratteri.</p>
     </header>
 </body>
 </html>
